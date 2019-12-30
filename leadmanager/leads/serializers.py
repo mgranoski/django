c82213b2754e6970f386f8359361c492341cb60b
Michael Granoski <mgranoski@U8011992-TPL-A.ten.thomsonreuters.com>
2019-12-30 17:02:06 -0600

    final state of project

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-from leads.models import Lead
+from leads.models import Lead 
 
-# Lead Seralizer
-
+# Lead Serializer
 class LeadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lead
-        fields = '__all__'
-        
+  class Meta:
+    model = Lead 
+    fields = '__all__'
